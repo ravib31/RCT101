be58f3229ea1e0c5e053a8379e56a63d6bc8cb2d
atheist31 <rkb0808@gmaol.com>
2022-12-17 00:32:16 +0530

    todo with json server

--- state management/src/Components/AllTodo.jsx
+++ state management/src/Components/AllTodo.jsx
@@ -4,7 +4,7 @@ import SingleTodo from './SingleTodo';
 export const AllTodo = ({todos , handleUpdate , handleTodoDelete}) => {
   return (
     <div style={{bg:"yellow",color:"red" ,paddingLeft:"10px"}}> 
-   {todos.map((todo) => <SingleTodo {...todo} handleUpdate={handleUpdate} handleTodoDelete={handleTodoDelete}/>)}
+   {todos ?.map((todo) => <SingleTodo {...todo} handleUpdate={handleUpdate} handleTodoDelete={handleTodoDelete}/>)}
     </div>
   )
 }
--- state management/src/Components/Todo.jsx
+++ state management/src/Components/Todo.jsx
@@ -3,32 +3,58 @@ import { useState } from 'react'
 import { AllTodo } from './AllTodo'
 import Navbar from './Navbar'
 import TodoInput from './TodoInput'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 const Todo = () => {
     const [title,setTitle] = useState("Todo App");
-    const [todos,setTodos] = useState([]);
+    const [todos,setTodos] = useState(null);
+
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+    const getData=()=>{
+        axios.get("http://localhost:3000/todos").then((res) =>{
+            setTodos(res.data);
+    })
+    }
     const addTodo= (data)=>{
         const payload={
             title: data,
             id:Date.now(),
             status:false,
         }
-        setTodos([...todos,payload])
+       axios.post ("http://localhost:3000/todos",payload).then((res) =>{
+        alert("Added Successfully");
+        getData();
+    }).catch((err) =>{
+        alert("Failed to add")
+    
+       })
     }
-    const handleUpdate=(id)=>{
-        const newTodos =todos.map((todo) =>{
-            if(todo.id===id){
-                return{...todo,status:!todo.status}
-            }
-            else{
-                return todo;
-            }
+    const handleUpdate=(id,status)=>{
+        // const newTodos =todos.map((todo) =>{
+        //     if(todo.id===id){
+        //         return{...todo,status:!todo.status}
+        //     }
+        //     else{
+        //         return todo;
+        //     }
+        // })
+        // setTodos(newTodos)
+        axios.patch (`http://localhost:3000/todos/${id}`,{status: !status}).then((res)=>{
+            alert("Data Updated");
+            getData();
         })
-        setTodos(newTodos)
+
     }
     const handleTodoDelete= (id)=>{
-        const newTodos =todos.filter((todo) => todo.id !== id);
-        setTodos(newTodos);
+        axios.delete(`http://localhost:3000/todos/${id}`).then((res)=>{
+            alert("Deleted Successfully");
+            getData();
+        })
     }
   return (
     <div>
